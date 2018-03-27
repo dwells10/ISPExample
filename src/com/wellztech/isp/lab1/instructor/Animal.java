c62ccfd61c102f9870bad50057c44f9be9ed0ece
dillonwells <dwells10@my.wcyc.edu>
2018-03-27 18:34:59 -0500

    Initial commit

---
+++ src/com/wellztech/isp/lab1/instructor/Animal.java
@@ -0,0 +1,12 @@
+package com.wellztech.isp.lab1.instructor;
+
+public interface Animal {
+
+    public void talk(String s);
+    public void run(int distance);
+    public void jump();
+    public void hike();
+    public void swim();
+    public void trip();
+
+}
---
+++ src/com/wellztech/isp/lab1/instructor/Vehicle.java
@@ -0,0 +1,10 @@
+package com.wellztech.isp.lab1.instructor;
+
+public interface Vehicle {
+
+    public void start();
+    public void drive(int distance);
+    public void stopByBreaking();
+
+
+}
